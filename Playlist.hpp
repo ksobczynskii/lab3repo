76d59ac32c1f5dfa36036750345115bf02b2a43a
Konstanty Sobczynski <01199334@pw.edu.pl>
2025-03-28 16:40:44 +0100

    dodalem deklaracje Playlist.hpp, trzeba podefiniowac

--- Playlist.hpp
+++ Playlist.hpp
@@ -9,4 +9,9 @@ private:
 
 public:
     Playlist(unsigned int init_cap = 10) : songs{}, capacity{init_cap}, size{0} {};
+    Playlist(const Playlist &other) : capacity{other.capacity}, size{other.size}, songs{other.songs} {};
+    Playlist &operator=(const Playlist &other);
+    Playlist(Playlist &&other);
+    Playlist &operator=(Playlist &&other);
+    ~Playlist();
 };
